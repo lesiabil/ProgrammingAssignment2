1dd2d51953c598f6a0ccb5dbef0d85d2d0e00c46
lesiabil <lbilitchenko@gmail.com>
2015-05-23 11:14:56 -0700

    Assignment 2
    Created makeCacheMatrix and cacheSolve functions to calculate the
    inverse of a matrix and return cached result if already exists in cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions calculate the inverse of a matrix and cache the result.
+## Caching is achieved by utilizing R’s lexical scoping environment.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a list of following four functions
+## sets the matrix to passed in parameter matrix and the inverse value to null
+## gets the matrix
+## sets the inverse matrix to passed in inverse matrix parameter
+## gets the inverse matrix
 
+makeCacheMatrix <- function(xMat = matrix()) {
+	inverse <- NULL
+	set <- function(yMat) {
+		xMat <<- yMat
+		inverse <<- NULL
+	}
+	get <- function() xMat
+	setInverse <- function(i) inverse <<- i
+	getInverse <- function() inverse
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) # returns vector type of list of functions
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of a matrix if it does not already exist in the cache.
+## Otherwise the function returns the cached value.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(xMakeMatrix, ...) {
+        iInverse <- xMakeMatrix$getInverse() # returns the Inverse of the special function vector
+	
+	# if an inverse exists in cached environment, return the value and leave the function
+	if(!is.null(iInverse)){
+		message("getting cached inverse matrix")
+		return(iInverse)
+	}
+	# otherwise if inverse was not cached, compute the inverse and store in cache
+	data <- xMakeMatrix$get()
+	iInverse <- solve(data, ...)
+	xMakeMatrix$setInverse(iInverse)
+	return(iInverse)
 }
